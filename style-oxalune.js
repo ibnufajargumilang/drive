b0f8cbbd716632b42ca965325c347fe1f56d5e0a
Ibnu Fajar Gumilang <105407156+ibnufajargumilang@users.noreply.github.com>
2024-09-16 20:17:42 +0700

    Create style-oxalune.js

---
+++ style-oxalune.js
@@ -0,0 +1,27 @@
+.fade-in {
+    animation: fadeIn 1s ease-in-out;
+}
+
+@keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+
+body {
+    background-image: url('https://i.ibb.co/9Tg8KrF/whatsapp-bg.png');
+}
+.expired-label {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: red;
+    color: white;
+    padding: 4px 8px;
+    font-size: 12px;
+    font-weight: bold;
+    border-bottom-left-radius: 8px;
+}
